02bd9933308792667deba185f9f1ca4ead890e58
Varun Sharma <varunsh@stepsecurity.io>
2023-11-30 12:36:48 -0800

    Add more files

--- dist/index.js
+++ dist/index.js
@@ -30990,53 +30990,72 @@ var __webpack_exports__ = {};
 "use strict";
 
 
-const { promisify } = __nccwpck_require__(1669)
-const path = __nccwpck_require__(5622)
-const os = __nccwpck_require__(2087)
-const fs = __nccwpck_require__(5747)
+const { promisify } = __nccwpck_require__(1669);
+const path = __nccwpck_require__(5622);
+const os = __nccwpck_require__(2087);
+const fs = __nccwpck_require__(5747);
 
-const cache = __nccwpck_require__(7784)
-const core = __nccwpck_require__(2186)
+const cache = __nccwpck_require__(7784);
+const core = __nccwpck_require__(2186);
 
-const chmod = promisify(fs.chmod)
+const chmod = promisify(fs.chmod);
 
 if (require.main === require.cache[eval('__filename')]) {
-  main().catch(err => {
-    console.error(err.stack)
-    process.exit(1)
-  })
+  main().catch((err) => {
+    console.error(err.stack);
+    process.exit(1);
+  });
 }
 
-async function main () {
+async function validateSubscription() {
+  const API_URL = `https://agent.api.stepsecurity.io/v1/github/${process.env.GITHUB_REPOSITORY}/actions/subscription`;
+
   try {
-    const url = core.getInput('yq-url')
-    const version = core.getInput('yq-version')
-    const platform = os.platform()
-    let arch = os.arch()
-    if (arch === 'x64') {
-      arch = 'amd64'
+    await axios.get(API_URL, { timeout: 3000 });
+  } catch (error) {
+    if (error.response) {
+      console.error(
+        "Subscription is not valid. Reach out to support@stepsecurity.io"
+      );
+      process.exit(1);
+    } else {
+      core.info("Timeout or API not reachable. Continuing to next step.");
+    }
+  }
+}
+
+async function main() {
+  try {
+    await validateSubscription();
+
+    const url = core.getInput("yq-url");
+    const version = core.getInput("yq-version");
+    const platform = os.platform();
+    let arch = os.arch();
+    if (arch === "x64") {
+      arch = "amd64";
     }
 
-    let toolPath = cache.find('yq', version, arch)
+    let toolPath = cache.find("yq", version, arch);
 
     if (!toolPath) {
       const context = {
         arch,
         platform,
-        version
-      }
+        version,
+      };
       const rendered = url.replace(/\{(\w+?)\}/g, (a, match) => {
-        return context[match] || ''
-      })
+        return context[match] || "";
+      });
 
-      const downloadPath = await cache.downloadTool(rendered)
-      toolPath = await cache.cacheFile(downloadPath, 'yq', 'yq', version)
+      const downloadPath = await cache.downloadTool(rendered);
+      toolPath = await cache.cacheFile(downloadPath, "yq", "yq", version);
     }
 
-    await chmod(path.join(toolPath, 'yq'), 0o755) // just in case we haven't preserved the executable bit
-    core.addPath(toolPath)
+    await chmod(path.join(toolPath, "yq"), 0o755); // just in case we haven't preserved the executable bit
+    core.addPath(toolPath);
   } catch (error) {
-    core.setFailed(error.message)
+    core.setFailed(error.message);
   }
 }
 
